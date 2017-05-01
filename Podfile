e9c76ed698abdb6a23ff03f7a3c076bab4768e30
Maciej Oczko <maciek.oczko@polidea.com>
2017-05-01 09:52:25 +0200

    Add Marshal, SnapKit & RxDataSources as pods

--- Podfile
+++ Podfile
@@ -5,6 +5,9 @@ platform :ios, '10.0'
 use_frameworks!
 
 target 'RxSwiftWorkshop' do
-    pod 'RxSwift',    '~> 3.0'
-    pod 'RxCocoa',    '~> 3.0'
+    pod 'RxSwift', '~> 3.0'
+    pod 'RxCocoa', '~> 3.0'
+    pod 'RxDataSources', '~> 1.0'
+    pod 'SnapKit', '~> 3.2.0'
+    pod 'Marshal', '~> 1.0'
 end
--- Pods/Target Support Files/Pods-RxSwiftWorkshop/Pods-RxSwiftWorkshop-frameworks.sh
+++ Pods/Target Support Files/Pods-RxSwiftWorkshop/Pods-RxSwiftWorkshop-frameworks.sh
@@ -89,12 +89,18 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Marshal/Marshal.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxCocoa/RxCocoa.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RxDataSources/RxDataSources.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxSwift/RxSwift.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SnapKit/SnapKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Marshal/Marshal.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxCocoa/RxCocoa.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RxDataSources/RxDataSources.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxSwift/RxSwift.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SnapKit/SnapKit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
